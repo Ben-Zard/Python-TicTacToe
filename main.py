956579966dc69955c04733e9690adf27ac5f80e8
Ben-Zard <you@example.com>
2021-12-08 18:13:49 -0500

    2

--- main.py
+++ main.py
@@ -4,26 +4,60 @@ from IPython import clear_output
 def display_board(board):
     #append the board game with the input
 print('\n'*100)#clears out
+
+#Write a function that can print out a board. Set up your board as a list, where each index 1-9
+# corresponds with a number on a number pad, so you get a 3 by 3 board representation.
+
 #player input to assign player at X or O ()
 def players_input():
+
+#Write a function that can take in a player input and assign their marker as 'X' or 'O'.
+# Think about using while loops to continually ask until you get a correct answer.
+
 
 # place maker (board, marker, position)
 def place_maker(board,marker,position):
 
+#Write a function that takes in the board list object, a marker ('X' or 'O'),
+# and a desired position (number 1-9) and assigns it to the board.
+
 #choose player that goes first (random or something else no input peramiter maybe)
 def choose_first():
+
+#Write a function that uses the random module to randomly decide which player goes first.
+# You may want to lookup random.randint() Return a string of which player went first.
+
 
 #check if win
 def win_check(board,marker):
 
+#run the win_check function against our test_board - it should return True
+
 #player space input check
 def board_check(board,position):
+
+# Write a function that returns a boolean indicating whether a space on the board is freely available.
+
 
 #check if the board is full
 def full_board_check(board):
 
+# Write a function that checks if the board is full and returns a boolean value.
+# True if full, False otherwise.
+
+
+def player_choice(board):
+#Write a function that asks for a player's next position (as a number 1-9) and then uses the function from step 6 to check if it's a free position.
+# If it is, then return the position for later use.
+
+
 #if they want to play again
 def replay():
+
+#Write a function that asks the player if they want to play again and returns a boolean
+# True if they do want to play again.
+
+
 
 #run the main game
 def main():
