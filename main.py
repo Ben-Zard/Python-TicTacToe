60ede56070e44a6c5077e2f81f5c231185e63ad6
Ben-Zard <zardusb@mail.gvsu.edu>
2021-12-08 18:03:28 -0500

    3

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 #board game layout to display board game takes (board as input)
-#append the board game with the input
-#player input to assign player at X or O ()
-# place maker (board, marker, position)
-#check if win
-#player input check
-#if they want to play again
-#run the main game
+def display_board(board):
+    #append the board game with the input
 
+#player input to assign player at X or O ()
+def players_input():
+
+# place maker (board, marker, position)
+def place_maker(board,marker,position):
+
+#choose player that goes first (random or something else no input peramiter maybe)
+def choose_first():
+
+#check if win
+def win_check(board,marker):
+
+#player space input check
+def board_check(board,position):
+
+#check if the board is full
+def full_board_check(board):
+
+#if they want to play again
+def replay():
+
+#run the main game
 def main():
-#keep playing use else break
+    #keep playing use else break
