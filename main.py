7f7905322c73a315593e1b1f81a0c4165ab666e7
Ben-Zard <you@example.com>
2021-12-08 17:53:51 -0500

    Creating

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-
-#board game layout
+#board game layout to display board game takes (board as input)
 #append the board game with the input
-# player input
-
+#player input to assign player at X or O ()
+# place maker (board, marker, position)
+#check if win
 #player input check
-
+#if they want to play again
 #run the main game
 def main():
+#keep playing use else break
