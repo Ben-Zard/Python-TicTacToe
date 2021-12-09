56f929bf07e27c5015bf27f7597480a098fae968
Ben-Zard <you@example.com>
2021-12-09 12:59:44 -0500

    2

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-board = [' ' for x in range(10)]
+board = [f'{x}' for x in range(10)]
 #board game layout to display board game takes (board as input)
 def display_board(board):
-print(' | | ')
-print(f'{board[1]}| {board[2]}|{board[3]}')
-print(f'{board[4]}| {board[5]}|{board[6]}')
-print(f'{board[7]}| {board[8]}|{board[9]}')
-print(display_board(board))
+    print('   |   | ')
+    print(f' {board[1]} | {board[2]} | {board[3]}')
+    print('-----------')
+    print(f' {board[4]} | {board[5]} | {board[6]}')
+    print('-----------')
+    print(f' {board[7]} | {board[8]} | {board[9]}')
+    print('   |   | ')
 
+display_board(board)
 #append the board game with the input
 # print('\n'*100)#clears out
+
 # #Write a function that can print out a board. Set up your board as a list, where each index 1-9
 # # corresponds with a number on a number pad, so you get a 3 by 3 board representation.
 #
